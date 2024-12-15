9470f9a9f757836ef5b31f0497030659e2c2f40a
daniil karpov <middledkforwork@gmail.com>
2024-12-15 13:36:11 +0300

    DZ-week-5-task-1

--- lesson-01/01.js
+++ lesson-01/01.js
@@ -2,4 +2,10 @@
 // Результат присвойте переменной sum
 
 // your code
-let sum
+let sum = 0;
+let countNumber = 1;
+while (countNumber <= 20) {
+    sum += countNumber;
+    countNumber++
+    console.log(sum);
+}
