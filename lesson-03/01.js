bd7e03551465fd54c0db21db9d97c20fb097025e
daniil karpov <middledkforwork@gmail.com>
2025-01-04 20:49:19 +0300

    DZ-Week-7

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,5 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(number1, number2) {
+    return ((number1 + number2) / 2)
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,19 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(numberA, numberB, numberC ) {
+
+        if ((numberA >= numberB) && (numberA >= numberC) ) {
+    
+            return numberA
+    
+        } else if ((numberB >= numberC) && (numberB >= numberA)) {
+    
+            return numberB
+    
+        } else {
+    
+            return numberC
+    
+        }
+    
+    }
