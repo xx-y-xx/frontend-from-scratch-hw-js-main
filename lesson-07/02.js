0f26f92aef192b8cf9cba2518eebcdd7225a69cf
daniil karpov <middledkforwork@gmail.com>
2025-03-12 22:30:45 +0300

    Week-11-Task-2

--- lesson-07/02.js
+++ lesson-07/02.js
@@ -1,14 +1,20 @@
 /*
  * Напишите функцию isNumeric, которая принимает строку и проверяет, представляет ли эта строка корректное числовое значение.
  * Если строка является числом, функция должна возвращать true, в противном случае - false.
- */
+ 
 
-function isNumeric(str) {
+🔹function isNumeric(str) {
   // your code
 }
-
+*/
 // console.log(isNumeric("123")) // Ожидаемый результат: true
 // console.log(isNumeric("12.3")) // Ожидаемый результат: true
 // console.log(isNumeric("123abc")) // Ожидаемый результат: false
 // console.log(isNumeric("abc")) // Ожидаемый результат: false
 // console.log(isNumeric(" ")) // Ожидаемый результат: false
+
+function isNumeric(str) {  
+  str.trim()
+  const conversionNumber = +str; 
+  return !Number.isNaN(conversionNumber)
+}
